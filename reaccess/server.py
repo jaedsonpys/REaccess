0937b8eda8e16830e6d236b00318dc79cb371d54
Jaedson Silva <contacanal798@gmail.com>
2022-08-15 15:37:59 -0300

    adding method to run server

--- reaccess/server.py
+++ reaccess/server.py
@@ -22,3 +22,37 @@ class Server(object):
     def _wait_data(self, client: socket.socket) -> str:
         data = client.recv(1024)
         return data.decode()
+
+    def run(self) -> None:
+        client, addr = self._socket.accept()
+
+        if self._password:
+            client.send(b'required_password')
+            
+            while True:
+                password = self._wait_data(client)
+
+                if self._password == password:
+                    client.send(b'logged')
+                    break
+                else:
+                    client.send(b'incorrect')
+        else:
+            client.send(b'logged')
+
+        while True:
+            command = self._wait_data(client)
+        
+            process = subprocess.Popen(
+                command,
+                shell=True,
+                universal_newlines=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+
+            while process.poll() is None:
+                output = process.stdout.readline()
+                client.send(output.encode())
+            else:
+                client.send(b'finish')
